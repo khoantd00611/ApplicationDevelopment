c3991be94d374e90ce801eec50120901586f29ba
khoantd00611 <khoantd00611@fpt.edu.vn>
2018-12-04 10:11:17 +0700

    hoan thanh

---
+++ ApplicationDevelopment/Controllers/ExamController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ApplicationDevelopment.Controllers
+{
+    public class ExamController : Controller
+    {
+        ExamContext db = new ExamContext();
+        // GET: Exam
+        public ActionResult Index()
+        {
+            return View(db.Exams.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Create(Exam exam)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Exams.Add(exam);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(exam);
+        }
+    }
+}
